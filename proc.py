5698961fbc5b1b3787e117b9e7c99b26049f4fb4
Issa Rice <riceissa@gmail.com>
2017-12-15 13:31:21 -0800

    add script

---
+++ proc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# License: CC0 https://creativecommons.org/publicdomain/zero/1.0/
+
+import csv
+import datetime
+
+def mysql_quote(x):
+    '''
+    Quote the string x using MySQL quoting rules. If x is the empty string,
+    return "NULL". Probably not safe against maliciously formed strings, but
+    whatever; our input is fixed and from a basically trustable source..
+    '''
+    if not x:
+        return "NULL"
+    x = x.replace("\\", "\\\\")
+    x = x.replace("'", "''")
+    x = x.replace("\n", "\\n")
+    return "'{}'".format(x)
+
+def main():
+    with open("data.csv", "r") as f:
+        reader = csv.DictReader(f)
+
+        first = True
+
+        print("""insert into donations (donor, donee, amount, donation_date,
+        donation_date_precision, donation_date_basis, cause_area, url,
+        donor_cause_area_url, notes, affected_countries,
+        affected_regions) values""")
+
+        for row in reader:
+
+            amount = row['amount'].replace("$", "").replace(",", "")
+            donation_date = (datetime.datetime.strptime(row['date'],
+                             "%B %d, %Y").strftime("%Y-%m-%d"))
+
+            print(("    " if first else "    ,") + "(" + ",".join([
+                mysql_quote("Hewlett Foundation"),  # donor
+                mysql_quote(row['grantee']),  # donee
+                amount,  # amount
+                mysql_quote(donation_date),  # donation_date
+                mysql_quote("day"),  # donation_date_precision
+                mysql_quote("donation log"),  # donation_date_basis
+                mysql_quote(row['cause_area']),  # cause_area FIXME this needs to be mapped
+                mysql_quote("https://www.hewlett.org/grants/"),  # url
+                mysql_quote(row['cause_area_url']),  # donor_cause_area_url
+                mysql_quote(row['notes']),  # notes
+                # FIXME these are only listed on each grant page, and there are
+                # a HUGE number of grant pages
+                mysql_quote(""),  # affected_countries
+                mysql_quote(""),  # affected_regions
+            ]) + ")")
+            first = False
+        print(";")
+
+if __name__ == "__main__":
+    main()
